b3b1df1e128f0b88d6b67dc8584f0f941fbab8a6
Yuthala <dinapolousova@gmail.com>
2024-03-07 16:48:26 +0300

    increase, decrease functions, data array

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import data from './data.js';
+
 
 function App() {
+
+  const [cart, setCart] = useState(data);
+  console.log(data);
+
+	//функция увеличения количества товара в корзине
+	const increase = (id) => {
+
+		setCart((cart) => {
+			return cart.map((product) => {
+				if (product.id === id) {
+					return {
+						...product,
+							count: product.count + 1,
+							priceTotal: (product.count + 1) * product.price
+					};
+				}
+				return product;
+			})
+		})
+	}
+
+	//функция уменьшения количества товара в корзине
+	const decrease = (id) => {
+
+		setCart((cart) => {
+			return cart.map((product) => {
+				if (product.id === id) {
+
+					const newCount = product.count - 1 > 1 ? product.count - 1 : 1;
+
+					return {
+						...product,
+							count: newCount,
+							priceTotal: newCount * product.price
+					};
+				}
+				return product;
+			})
+		})
+	}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <h3>Счетчик: {counter}</h3> */}
+      <button onClick={increase}>Добавить</button>
+      <button onClick={decrease}>Убрать</button>
     </div>
   );
 }
